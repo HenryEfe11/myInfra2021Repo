ebd8adb71e51ae736a435c97ea1b36037cc256f4
HenryEfe11 <125158809+HenryEfe11@users.noreply.github.com>
2023-03-19 21:32:55 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "Newkey11" 
+    default     =  "Newkey03" 
 }
 
 variable "instance_type" { 
